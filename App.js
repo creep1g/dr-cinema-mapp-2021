07d4d45ac9014465f5a44c69a5d69810fb2ab11f
Toggi <thorgilshjalmarsson@gmail.com>
2021-12-06 15:09:30 +0000

    We get an access token woop woop !

--- App.js
+++ App.js
@@ -1,40 +1,31 @@
 import { StatusBar } from 'expo-status-bar';
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import { StyleSheet, Text, View } from 'react-native';
 
 export default function App() {
 	
-	const auth = () => {
-		const requestBody = {
-			method: 'POST',
-			headers: { 'Content-Type': 'application/json' },
-			body: JSON.stringify({ username: 'creep1g', password: 'Streetlock10' })
-		};
-		fetch('https://api.kvikmyndir.is/authenticate', requestBody).catch( (error) => console.log(error) )
-	};
+	const [token, setToken] = useState();
 
+// Get access token !
 
-	const token = 'eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJfaWQiOiI2MWFkZGNmNGExMTQ0MDFmNDU2NWRmOTQiLCJnbG9iYWxhZG1pbiI6ZmFsc2UsImFkbWluIjpmYWxzZSwiYWN0aXZlIjp0cnVlLCJmdWxsbmFtZSI6IsOeb3JnaWxzIMOBcm5pIEhqw6FsbWFyc3NvbiIsImVtYWlsIjoiY3JlZXAxZ0BnbWFpbC5jb20iLCJ1c2VybmFtZSI6ImNyZWVwMWciLCJwYXNzd29yZCI6IiQyYSQwOCRucExQV3hkL2RqS1E5U252ZFlrcHV1Q1ZjaVlRclRadHdEalEvYnRoZUM0OTNFWUMzNzlzeSIsImRvbWFpbiI6ImxvY2FsaG9zdCIsIm1lc3NhZ2UiOiJTY2hvb2wiLCJpYXQiOjE2Mzg3ODgzMjMsImV4cCI6MTYzODg3NDcyM30.vi67Eo8a6SXh8XgA-DOGO_xOSlioO4E-Nr6LrZZqgDQ'
-
-	const get = async (token) => {
-		const requestBody = {
-			method: 'GET',
-			headers: { "x-access-token": token,
-								 "Content-Type": "application/json"
-			},
-		};
-		const res = await fetch('http://api.kvikmyndir.is/movies', requestBody)
-			// .then( (response) => response.status )
-			// .then( (res) => console.log(res))
-			// .catch( (error) => console.log(error) )
-		const jso = await res.json();
-		console.log(jso)
-	};
+	useEffect( () => {
+		(async () => {
+			const requestBody = {
+				method: 'POST',
+				headers: { 'Content-Type': 'application/json' },
+				body: JSON.stringify({ 
+					username: 'Morgaes',
+					password: 'Morgaes1' })
+			};
+			fetch('https://api.kvikmyndir.is/authenticate/', requestBody)
+				.then( (response) => response.json() )
+				.then( (res) =>  setToken(res.token))
+				.catch( (error) => console.log(error) )
+		})();
+	}, []);
 	
-	// auth();
-	get(token);
 
-  return (
+	return (
     <View style={styles.container}>
       <Text>Open up App.js to start working on your app!</Text>
       <StatusBar style="auto" />
