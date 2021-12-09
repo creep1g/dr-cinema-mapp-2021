a8d20182f50bc6f5a64b0c5b3544a207a544d89f
Toggi <thorgilshjalmarsson@gmail.com>
2021-12-09 14:16:47 +0000

    Toolbar removed

--- src/views/Movies/index.js
+++ src/views/Movies/index.js
@@ -2,8 +2,6 @@ import React, { useEffect } from 'react';
 import { View, TouchableHighlight, Text } from 'react-native';
 import { useSelector} from 'react-redux';
 import MoviesList from '../../components/MoviesList';
-import Toolbar from '../../components/Toolbar';
-import {useDispatch} from 'react-redux';
 
 
 const Movies = function ( {route,  navigation: { navigate } } ) {
@@ -12,9 +10,6 @@ const Movies = function ( {route,  navigation: { navigate } } ) {
 	// This should be handled with redux.. i think?
 	return(
 		<View style={{ flex: 1 }}>
-			<Toolbar 
-			getUpcoming={() => navigate('Upcoming')}
-			/>
 			<MoviesList
 				onSelect={(movie) => onPressMovie(movie)} 
                 films={movies}/>
