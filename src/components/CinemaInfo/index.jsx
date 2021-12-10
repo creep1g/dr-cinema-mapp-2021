429718b634ef38017b934f569258b4d4230c79f8
Toggi <thorgilshjalmarsson@gmail.com>
2021-12-10 13:33:57 +0000

    Some minor linting in cinema info and cinemas view

--- src/components/CinemaInfo/index.jsx
+++ src/components/CinemaInfo/index.jsx
@@ -1,25 +1,24 @@
-import React from 'react-native';
-import { View, TouchableHighlight, Text } from 'react-native';
-import { useSelector } from 'react-redux';
-import styles from './styles';
+import React, {Text, View} from 'react-native';
 import body from '../../styles/body';
 
-const CinemaInfo = function ( {openUrl, openJa, openPhone } ) {
-  const movies = useSelector(state => state.movies);
-  const cinema = useSelector(state => state.cinema);
-  const url = "http://" + cinema.website;
+const CinemaInfo = function( ) {
+  const cinema = useSelector((state) => state.cinema);
   const regex = /(<([^>]+)>)/ig;
-	
-  return(
-	<View style={ [ body.body, { flex:1, justifyContent:'center', alignItems: 'center'} ] }>
-		<View>
-			{
-			  cinema.description !== null
-			  ? <Text > { cinema.description.replace(regex, '\n')  } </Text>
-			  :<></>
-			}
-		</View>
-	</View>
+
+  return (
+    <View style={
+      [body.body,
+        {flex: 1,
+          justifyContent: 'center',
+          alignItems: 'center'}] }>
+      <View>
+        {
+         cinema.description !== null?
+         <Text > { cinema.description.replace(regex, '\n') } </Text>:
+         <></>
+        }
+      </View>
+    </View>
   );
 };
 
